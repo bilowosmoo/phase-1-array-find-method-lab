5e9c3a2a8adf16849cff44cbc582acc2fed051d2
bilow osmoo <bilowosmo@gmail.com>
2025-03-13 22:09:08 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin(records) {
+    const win = records.find(record => record.result === "W");  // Find the first win
+    if (win) {
+      return win.year; 
+    } else {
+      return undefined; 
+    }
+  }
+  
+  
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+  
+  console.log(superbowlWin(record)); 
